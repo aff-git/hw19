93384fd1924b391afab8c590196a2dafe1ba2ebb
Prototypic <aff4ads@gmail.com>
2024-09-04 16:08:05 +0300

    update

--- assets/js/a2.js
+++ assets/js/a2.js
@@ -1,8 +1,9 @@
 let year = +prompt("Enter year:");
 
-if (year % 4 == 0) {
-    console.log(`Введенный год ${year} - высокосный год`);
-} else {console.log(`Введенный год ${year} - обычный год`);}
-
-
-// Реализовано через деление на 4 без остатка
+if (year % 100 == 0 && year % 400 !== 0 ) {
+    console.log(`Введенный год ${year} - обычный год (исключение)`);
+} else if (year % 400 == 0 || year % 4 == 0)
+    {console.log(`Введенный год ${year} - Высокосный год`);
+} else
+    {console.log(`Введенный год ${year} - обычный год`);
+}
